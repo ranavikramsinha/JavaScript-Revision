18e41a1d2c82a5e566ef60a3a4e5c46a0c24d80b
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-21 21:46:38 +0530

    node environment

---
+++ 01/01_first.js
@@ -0,0 +1,25 @@
+console.log(4+5)
+console.log("Rana Vikram Sinha")
+
+const fs = require("fs")
+
+//* Define two variables
+let a = 10
+let b = 5
+
+//* Basic arithmetic operations
+let sum = a + b
+let product = a * b
+
+//* Prepare data to write
+let data = `Sum: ${sum}\nProduct: ${product}`
+console.log(data)
+
+//* Write data to a local file
+fs.writeFile("Output.txt", data, (err) => {
+    if(err){
+        throw err
+    }
+
+    console.log("Data written to file")
+})
