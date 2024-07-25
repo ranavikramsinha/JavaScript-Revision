c695f32f22f71a537bc70092928061b90b980c6d
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-26 00:49:40 +0530

    if esle and truthy & falsy values

---
+++ 03_decision_control/if_else_and_truthy_&_falsy_values.js.js
@@ -0,0 +1,15 @@
+if(1){
+    console.log('truthy')
+}
+else{
+    console.log('falsy')
+}
+
+let a = undefined; // falsy values: null, 0, undefined, NaN, false, ""
+
+if(a){
+    console.log('truthy')
+}
+else{
+    console.log('falsy')
+}
