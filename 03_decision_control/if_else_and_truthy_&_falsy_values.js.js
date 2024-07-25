474cb8040ac0b673369dc96c577666f2b5f50530
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-26 01:27:25 +0530

    if else

--- 03_decision_control/if_else_and_truthy_&_falsy_values.js.js
+++ 03_decision_control/if_else_and_truthy_&_falsy_values.js.js
@@ -1,3 +1,4 @@
+// turthy values: values other than falsy values are the truthy values
 if(1){
     console.log('truthy')
 }
---
+++ 03_decision_control/nested_if_else.js
@@ -0,0 +1,16 @@
+let num = 88;
+
+if(num > 0){
+    if(num % 2 === 0){
+        console.log("Positive and Even Number")
+    }
+    else{
+        console.log("Positive and Odd Number")
+    }
+}
+else if(num < 0){
+    console.log("Negative Number")
+}
+else{
+    console.log("Number is Zero")
+}
