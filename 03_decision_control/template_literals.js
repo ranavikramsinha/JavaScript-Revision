f0876ff8cffc647a7b50715a26b40671ae368f03
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-26 19:40:36 +0530

    template literals

---
+++ 03_decision_control/template_literals.js
@@ -0,0 +1,17 @@
+let str1 = "Aryan";
+console.log(str1);
+
+let str2 = 'Rudraksha';
+console.log(str2);
+
+let name1 = 'Rana Vikram Sinha'
+let a = 5;
+let b = 5;
+let str3 = `${name1}
+
+Aryan Sinha
+
+        Rudraksha
+
+${a + b}`;
+console.log(str3);
