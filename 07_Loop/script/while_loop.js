6f00d5c95beec941037d365b9cf0cdc4fc8cb4ab
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-07 23:46:24 +0530

    while loop and prompt

--- 07_Loop/script/while_loop.js
+++ 07_Loop/script/while_loop.js
@@ -8,3 +8,29 @@ while(num <= 5){ //* condition
 };
 
 console.log(`Loop Ends`);
+
+function printMultiplicationTable(num){
+    let i = 1;
+
+    while(i < 11){
+        console.log(`${num} X ${i} = ${num * i}`);
+        i++;
+    }
+}
+
+//* let num2 = prompt('Enter the number for which table has to be generated:')
+let num2 = 8;
+printMultiplicationTable(num2);
+
+function getFactorial(num){
+    let j = 1;
+    let result = 1;
+
+    while(j <= num){
+        console.log(`${j}! = ${result *= j}`);
+        j++
+    }
+}
+
+let num3 = 10;
+getFactorial(num3);
