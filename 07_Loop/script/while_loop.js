7fa2bb0c15b2c5a1a7e73072aae399569b70f790
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-07 23:26:35 +0530

    while loop

---
+++ 07_Loop/script/while_loop.js
@@ -0,0 +1,10 @@
+console.log(`Loop Starts`);
+
+let num = 1; //* initialization
+
+while(num <= 5){ //* condition
+    console.log(`Number:${num}`)
+    num++; //* update
+};
+
+console.log(`Loop Ends`);
