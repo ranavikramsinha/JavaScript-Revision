b0141d19d00bfc917aa118e79700bfced80d3407
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-08 02:09:18 +0530

    coercion

---
+++ 08_coercion/script/coercion.js
@@ -0,0 +1,17 @@
+console.log("Hi" + 77);
+console.log("66" - 55);
+console.log(true == 1);
+console.log(true == 2);
+console.log(false == 0);
+console.log(true === 1);
+console.log(true === 2);
+
+//* 1. Memory Implicit Conversion: JavaScript automatically converts one data type to another when required.
+
+//* 2. String Conversion: Numbers and booleans can be implicitly converted to strings.
+
+//* 3. Number Conversion: Strings and
+//* booleans can be implicitly converted to
+//* numbers.
+
+//* 4. Boolean Conversion: Various data types can be converted to booleans (e.g., 0, "", null, undefined are false).
