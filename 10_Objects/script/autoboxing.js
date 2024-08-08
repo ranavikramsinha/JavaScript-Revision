e089824f91f901ea95aa0fddfb8caa99f4ce935a
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-09 00:49:44 +0530

    autoboxing

--- 10_Objects/script/autoboxing.js
+++ 10_Objects/script/autoboxing.js
@@ -1,17 +1,20 @@
 //* Primitive string
-let primitiveString="Hello, World!";
+let primitiveString = "Hello, World!";
 //* JavaScript automatically converts the primitive string to a String object
-let length=primitiveString.length;
+let length = primitiveString.length;
 console.log(`Length of string: ${length}`);
 
 //* Primitive number
-let primitiveNumber=42;
+let primitiveNumber = 42;
 //* JavaScript automatically converts the primitive number to a Number object
-let fixedNumber=primitiveNumber.toFixed(2);
+let fixedNumber = primitiveNumber.toFixed(2);
 console.log(`Fixed number: ${fixedNumber}`);
 
 //* Primitive boolean
-let primitiveBoolean=true;
+let primitiveBoolean = true;
 //* JavaScript automatically converts the primitive booleanto a Boolean object
-let booleanString=primitiveBoolean.toString();
+let booleanString = primitiveBoolean.toString();
 console.log(`Boolean as string: ${booleanString}`);
+
+let str1 = 'Aryan Sinha';
+console.log(str1.toUpperCase());
