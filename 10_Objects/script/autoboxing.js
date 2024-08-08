b4913f7a2f68bd727ca683507bf078d5fd9074f8
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-09 00:44:08 +0530

    autoboxing

--- 10_Objects/script/autoboxing.js
+++ 10_Objects/script/autoboxing.js
@@ -2,16 +2,16 @@
 let primitiveString="Hello, World!";
 //* JavaScript automatically converts the primitive string to a String object
 let length=primitiveString.length;
-console.log(`Length of string: ${length}`); //* Output: Length of string: 13
+console.log(`Length of string: ${length}`);
 
 //* Primitive number
 let primitiveNumber=42;
 //* JavaScript automatically converts the primitive number to a Number object
 let fixedNumber=primitiveNumber.toFixed(2);
-console.log(`Fixed number: ${fixedNumber}`); //* Output: Fixed number: 42.00
+console.log(`Fixed number: ${fixedNumber}`);
 
 //* Primitive boolean
 let primitiveBoolean=true;
 //* JavaScript automatically converts the primitive booleanto a Boolean object
 let booleanString=primitiveBoolean.toString();
-console.log(`Boolean as string: ${booleanString}`); // Output: Boolean as string: true
+console.log(`Boolean as string: ${booleanString}`);
