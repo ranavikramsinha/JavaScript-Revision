aaa8df70c4ca91a652c78c9d87ef4b64dbf1d7c3
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-13 23:25:16 +0530

    array

---
+++ 11_Arrays/script/array.js
@@ -0,0 +1,36 @@
+let students = [
+{
+    firstName: 'Aryan',
+    lastName: 'Sinha',
+    age: 17,
+    rollNo: 1,
+},
+{
+    firstName: 'Raja',
+    lastName: 'Babu',
+    age: 17,
+    rollNo: 2,
+},
+{
+    firstName: 'Rana Vikram',
+    lastName: 'Sinha',
+    age: 17,
+    rollNo: 3,
+},
+]
+
+console.log(students);
+console.log(students[0]);
+console.log(students[0].firstName);
+console.log(students[0].lastName);
+console.log(students[1].age);
+console.log(students[2].rollNo);
+
+let arr = [];
+console.log(typeof arr);
+
+let primeNumber = [2, 3, 5];
+
+for(let i = 0; i < primeNumber.length; i++){
+    console.log(primeNumber[i]);
+}
