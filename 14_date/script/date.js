59c30e66105c76f62ee03760b3dcd15307e52864
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-15 17:20:35 +0530

    date

---
+++ 14_date/script/date.js
@@ -0,0 +1,7 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getDay());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
