438e7807ed32d23a189d1a43a7574b768e797939
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-17 00:18:08 +0530

    object copy (shallow copy & deep copy)

--- 15_objects_wquality/script/object_copy.js
+++ 15_objects_wquality/script/object_copy.js
@@ -20,6 +20,10 @@ console.log('obj1ShallowCopy', obj1);
 console.log('obj1DeepCopy', obj1DeepCopy);
 console.log('Obj1', obj1);
 
+
+//* NOTE:- ShallowCopyForNestedObject is a valid implementation for creating a shallow copy, while shallowCopy merely returns the original reference without copying
+
+//* shallowCopy Function: It does not actually create a new object but rather returns the same reference, so there's no actual copying happening
 function shallowCopy(obj) {
   let obj2 = obj;
   return obj2;
@@ -41,6 +45,7 @@ obj2ShallowCopyForNestedObject.a.b = 700;
 console.log('obj2ShallowCopyForNestedObject', obj2ShallowCopyForNestedObject);
 console.log('Obj2', obj2);
 
+//* ShallowCopyForNestedObject Function: It creates a new object with the same top-level properties but maintains shared references for nested objects or arrays. This function performs a true shallow copy of the object structure
 function ShallowCopyForNestedObject(obj){
     return{...obj}
 }
